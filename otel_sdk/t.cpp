e7faae8ebfe6503e5e7c32f6435058ba40d19305
malkia <malkia@gmail.com>
2026-04-01 09:57:45 -0700

    tests with otelcol

--- otel_sdk/t.cpp
+++ otel_sdk/t.cpp
@@ -7,6 +7,7 @@
 #include <opentelemetry/exporters/ostream/metric_exporter_factory.h>
 #include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
 #include <opentelemetry/semconv/incubating/process_attributes.h>
+#include <opentelemetry/semconv/service_attributes.h>
 
 int main(int argc, const char* argv[])
 {
@@ -19,7 +20,7 @@ int main(int argc, const char* argv[])
 
   {
     using namespace opentelemetry::sdk::common::internal_log;
-    GlobalLogHandler::SetLogLevel(LogLevel::Debug);
+//    GlobalLogHandler::SetLogLevel(LogLevel::Debug);
   }
 
   {
@@ -27,7 +28,8 @@ int main(int argc, const char* argv[])
     using namespace sdk::resource;
     ResourceAttributes resources
     {
-       { semconv::process::kProcessVpid, index },
+       { semconv::process::kProcessPid, (int64_t)GetCurrentProcessId() },
+       { semconv::service::kServiceName, "t" },
     };
     auto resource = Resource::Create( resources );
     Resource::GetDefault() = resource;
@@ -44,25 +46,27 @@ int main(int argc, const char* argv[])
     auto stdoutReader{ opentelemetry::sdk::metrics::PeriodicExportingMetricReaderFactory::Create(std::move(stdoutExporter), options) };
     auto meterProvider{ opentelemetry::sdk::metrics::MeterProviderFactory::Create() };
     meterProvider->AddMetricReader(std::move(reader));
-    meterProvider->AddMetricReader(std::move(stdoutReader));
+//    meterProvider->AddMetricReader(std::move(stdoutReader));
     opentelemetry::metrics::Provider::SetMeterProvider(std::move(meterProvider));
   }
 
   {
     const auto apiMetricProvider{ opentelemetry::metrics::Provider::GetMeterProvider() };
-    auto meter{ apiMetricProvider->GetMeter("malkia_test_meter", "1.2.0") };
-    auto counter{ meter->CreateDoubleCounter("malkia_test_counter") };
+    auto meter{ apiMetricProvider->GetMeter("MEETAR", "666") };
+//    auto meter{ apiMetricProvider->GetMeter("", "") };
+    auto counter{ meter->CreateUInt64Counter("malkia_test_counter") };
     srand(index);
     bool flip{ false };
     int period{ 0 };
-    for(size_t i=0; i<320; i++) 
+    for(size_t i=0; i<64; i++) 
     {
       flip = !flip;
       if( flip )
-        period = rand()%160;
+        period = rand()%100;
+      printf("."); fflush(stdout);
       counter->Add(1,{{"test", "one"}});
       using namespace std::chrono_literals;
-      std::this_thread::sleep_for(std::chrono::milliseconds(flip ? period : 160 - period));
+      std::this_thread::sleep_for(std::chrono::milliseconds(flip ? period : 100 - period));
     }
   }
 
@@ -70,17 +74,17 @@ int main(int argc, const char* argv[])
     const auto sdkMetricsProvider{ std::dynamic_pointer_cast<opentelemetry::sdk::metrics::MeterProvider>( opentelemetry::metrics::Provider::GetMeterProvider() ) };
     if( sdkMetricsProvider )
     {
-      printf("\n  ... found sdk metrics provider!\n");
+//      printf("\n  ... found sdk metrics provider!\n");
       if( const auto p{ sdkMetricsProvider.get() }; p )
       {
-        printf("\n  ... force flushing it\n\n");
+  //      printf("\n  ... force flushing it\n\n");
         p->ForceFlush();
-        printf("\n  ... shutting it down\n\n");
-        p->Shutdown();
+    //    printf("\n  ... shutting it down\n\n");
+  //      p->Shutdown();
       }
     }
   }
  
-  printf("Done!\n");
+//  printf("Done!\n");
   return 0;
 }
