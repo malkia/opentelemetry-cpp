f4c36c59688cc8fd8331d187e85adda695e224b4
malkia <malkia@gmail.com>
2026-04-06 08:52:55 -0700

    some more experiments on aggregation

--- otel_sdk/m.cpp
+++ otel_sdk/m.cpp
@@ -1,100 +1,40 @@
 #include <windows.h>
-
-#include <reproc++/drain.hpp>
-#include <reproc++/reproc.hpp>
-
+#include <direct.h>
+#include <processenv.h>
 #include <atomic>
 #include <cstdbool>
 #include <future>
+#include <optional>
 #include <vector>
-#include <direct.h>
-#include <processenv.h>
 
-std::atomic<bool> g_started;
-std::atomic<bool> g_stop;
+#include <tinyproc/process.hpp>
 
-//#define fflush
-
+static std::atomic<bool> g_started;
+static std::unique_ptr<TinyProcessLib::Process> g_otelcol_proc;
 static char g_sourceDir[1024]{};
 
-int run_proc(const std::string &exe, const std::vector<std::string> &arguments)
+static void redirect_stdout(const char *bytes, size_t n)
 {
-  std::vector<const char *> args;
-
-  args.push_back(exe.c_str());
-  for (const auto &arg : arguments)
-    args.push_back(arg.c_str());
-  args.push_back(nullptr);
-
-  reproc::process proc;
-  reproc::options opts;
-  if( g_sourceDir[0] ) opts.working_directory = g_sourceDir;
-  opts.stop = {{reproc::stop::terminate, reproc::milliseconds(1000)},
-               {reproc::stop::kill, reproc::milliseconds(500)},
-               {}};
-
-  {
-    const auto ec{proc.start(args.data(), opts)};
-    if (ec)
-    {
-      printf("ERROR: %s\n", ec.message().c_str());
-      return ec.value();
-    }
-  }
-
-  std::mutex mutex;
-  std::string stdout_buf;
-  std::string stderr_buf;
-
-  const auto flush{[&stdout_buf, &stderr_buf, &mutex, &proc, &opts] {
-    std::lock_guard<std::mutex> lock(mutex);
-    fputs(stdout_buf.c_str(), stdout);
-    fputs(stderr_buf.c_str(), stderr);
-    stdout_buf.clear();
-    stderr_buf.clear();
-    //fflush(stdout);
-    //fflush(stderr);
-    g_started = true;
-  }};
-
-  auto drain_async{std::async(std::launch::async, [&proc, &stdout_buf, &stderr_buf, &mutex] {
-    reproc::sink::thread_safe::string sink_stdout(stdout_buf, mutex);
-    reproc::sink::thread_safe::string sink_stderr(stderr_buf, mutex);
-    return reproc::drain(proc, sink_stdout, sink_stderr);
-  })};
-
-  while (drain_async.wait_for(std::chrono::milliseconds(16)) != std::future_status::ready)
-  {
-    flush();
-    if (g_stop)
-    {
-      printf("Aborting...\n");
-      //    proc.terminate();
-      proc.kill();
-      printf("Killed...\n");
-      return 0;
-    }
-  }
-  flush();
-
-  auto ec{drain_async.get()};
-  if (ec)
-  {
-    printf("ERROR: %s\n", ec.message().c_str());
-    return ec.value();
-  }
-
-  int status{};
-  std::tie(status, ec) = proc.stop(opts.stop);
-  if (ec)
-  {
-    printf("ERROR: %s\n", ec.message().c_str());
-    return ec.value();
-  }
-  return status;
+  fwrite(bytes, 1, n, stdout);
 }
 
-std::string get_dir(std::string name)
+static void redirect_stderr(const char *bytes, size_t n)
+{
+  fwrite(bytes, 1, n, stderr);
+}
+
+static std::unique_ptr<TinyProcessLib::Process> run_proc(const std::vector<std::string> &args, bool customRedirect)
+{
+  TinyProcessLib::Config config;
+  config.inherit_file_descriptors = true;
+  // config.show_window = TinyProcessLib::Config::ShowWindow::force_minimize;
+  return std::make_unique<TinyProcessLib::Process>(args, g_sourceDir,
+                                                   customRedirect ? redirect_stdout : nullptr,
+                                                   customRedirect ? redirect_stderr : nullptr,
+                                                   false, config);
+}
+
+static std::string get_dir(std::string name)
 {
   const auto s1{name.rfind('\\')};
   const auto s2{name.rfind('/')};
@@ -105,83 +45,78 @@ std::string get_dir(std::string name)
 
 int main(int argc, const char *argv[])
 {
+  setvbuf(stdout, nullptr, _IONBF, 0);
+  setvbuf(stderr, nullptr, _IONBF, 0);
+  setvbuf(stdin, nullptr, _IONBF, 0);
+
   {
+    // when running under bazel, this would be the source directory
     size_t x{};
     ::getenv_s(&x, g_sourceDir, "BUILD_WORKING_DIRECTORY");
   }
-  
-  if (argc != 4 ) {
+
+  if (argc != 4)
+  {
     perror("exactly 3 arguments expected");
     return 1;
   }
 
-  setvbuf(stdout, nullptr, _IONBF, 0);
-  setvbuf(stderr, nullptr, _IONBF, 0);
   const auto this_exe_dir{get_dir(argv[0])};
   const auto otelcol_exe{this_exe_dir + std::string(argv[1])};
   const auto otelcol_yaml{this_exe_dir + std::string(argv[2])};
+
   printf("otelcol_exe=[%s]\n", otelcol_exe.c_str());
   printf("otelcol_yaml=[%s]\n", otelcol_yaml.c_str());
   printf("\n");
-  //fflush(stdout);
 
   int otelcolExitCode{};
   std::thread otelcol_thread{[&otelcol_exe, &otelcol_yaml, &otelcolExitCode]() {
     printf("[STARTING] otelcol\n");
-    //fflush(stdout);
-    otelcolExitCode = run_proc(otelcol_exe, {"--config", otelcol_yaml});
-    printf("[FINISHED] otelcol\n");
-    //fflush(stdout);
+    g_otelcol_proc  = run_proc({otelcol_exe, "--config", otelcol_yaml}, false);
+    otelcolExitCode = g_otelcol_proc->get_exit_status();
+    printf("[FINISHED] otelcol, exitCode=%d\n", otelcolExitCode);
   }};
 
-  if( argv[3][0] >= '0' && argv[3][0] <= '9' )
+  if (argv[3][0] >= '0' && argv[3][0] <= '9')
   {
     auto sleep = std::stoi(argv[3]);
     printf("Sleeping for %d seconds...\n", sleep);
     std::this_thread::sleep_for(std::chrono::seconds(sleep));
-  }   
+  }
   else
   {
-    while( !g_started )
+    while (g_otelcol_proc == nullptr)
     {
-      printf("*"); //fflush(stdout);
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+      printf("*");  // fflush(stdout);
+      std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
     const auto test_exe{this_exe_dir + std::string(argv[3])};
     printf("\ntest_exe=[%s]\n\n", test_exe.c_str());
-    //fflush(stdout);
+
     std::vector<std::thread> threads;
     threads.reserve(100);
-
     for (int i = 0; i < 10; i++)
     {
       int testExitCode{};
-      threads.emplace_back([&test_exe, &testExitCode, i=i]() {
-        testExitCode = run_proc(test_exe, {});
-        printf("[FINISHED] test %d\n", i);
-        //fflush(stdout);
+      threads.emplace_back([&test_exe, &testExitCode, i = i]() {
+        auto p        = run_proc({test_exe}, true);
+        auto exitCode = p->get_exit_status();
+        printf("[FINISHED] test %d (%d)\n", i, exitCode);
       });
     }
 
-    for(auto& thread : threads )
+    for (auto &thread : threads)
       if (thread.joinable())
         thread.join();
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::this_thread::sleep_for(std::chrono::seconds(3));
   }
 
+  if (g_otelcol_proc)
+    g_otelcol_proc->kill();
 
-  g_stop = true;
   printf("END!\n");
-  // if (otelcol_thread.joinable())
-  //   otelcol_thread.join();
-
-  // if( otelcolExitCode )
-  //   return otelcolExitCode;
-
-  // if( testExitCode )
-  //   return testExitCode;
 
   return 0;
 }
--- otel_sdk/t.cpp
+++ otel_sdk/t.cpp
@@ -1,29 +1,46 @@
 #include <thread>
 
-#include <opentelemetry/exporters/ostream/metric_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
-
 #include <opentelemetry/metrics/provider.h>
-
-#include <opentelemetry/sdk/common/global_log_handler.h>
+#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
 #include <opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader_factory.h>
 #include <opentelemetry/sdk/metrics/meter_provider_factory.h>
-
 #include <opentelemetry/semconv/incubating/process_attributes.h>
 #include <opentelemetry/semconv/service_attributes.h>
 
+#include <opentelemetry/sdk/common/global_log_handler.h>
+
+static void metricsCleanup()
+{
+  using namespace opentelemetry;
+  const auto mp{ std::dynamic_pointer_cast<sdk::metrics::MeterProvider>(metrics::Provider::GetMeterProvider()) };
+  if( !mp )
+    return;
+  const auto p{ mp.get() };
+  if( !p )
+      return;
+  p->ForceFlush();
+  p->Shutdown();
+}
+
+struct MetricsCleanup
+{
+  ~MetricsCleanup()
+  {
+    metricsCleanup();
+  }
+};
+
+static MetricsCleanup g_metricsCleanup;
+
 int main(int argc, const char *argv[])
 {
-
-  int index = argc == 2 ? atoi(argv[1]) : -1;
-
-  setvbuf(stdout, nullptr, _IONBF, 0);
-  setvbuf(stderr, nullptr, _IONBF, 0);
-  setvbuf(stdin, nullptr, _IONBF, 0);
-
   {
-    using namespace opentelemetry::sdk::common::internal_log;
-    //    GlobalLogHandler::SetLogLevel(LogLevel::Debug);
+    setvbuf(stdout, nullptr, _IONBF,0);
+    setvbuf(stderr, nullptr, _IONBF,0);
+    setvbuf(stdin, nullptr, _IONBF,0);
+
+      // using namespace opentelemetry::sdk::common::internal_log;
+      // GlobalLogHandler::SetLogLevel(LogLevel::Debug);
   }
 
   {
@@ -37,49 +54,48 @@ int main(int argc, const char *argv[])
     Resource::GetDefault() = resource;
   }
 
-  // Initialize and set the global MeterProvider
   {
-    auto exporter{opentelemetry::exporter::otlp::OtlpGrpcMetricExporterFactory::Create({})};
-    auto stdoutExporter = opentelemetry::exporter::metrics::OStreamMetricExporterFactory::Create();
-    opentelemetry::sdk::metrics::PeriodicExportingMetricReaderOptions options;
+    using namespace opentelemetry;
+    
+    auto exporter{exporter::otlp::OtlpGrpcMetricExporterFactory::Create({})};
+
+    sdk::metrics::PeriodicExportingMetricReaderOptions options;
     options.export_interval_millis = std::chrono::milliseconds(100);
-    options.export_timeout_millis  = std::chrono::milliseconds(50);
-    auto reader{opentelemetry::sdk::metrics::PeriodicExportingMetricReaderFactory::Create(
-        std::move(exporter), options)};
-    auto stdoutReader{opentelemetry::sdk::metrics::PeriodicExportingMetricReaderFactory::Create(
-        std::move(stdoutExporter), options)};
-    auto meterProvider{opentelemetry::sdk::metrics::MeterProviderFactory::Create()};
+    options.export_timeout_millis = std::chrono::milliseconds(50);
+
+    auto reader{sdk::metrics::PeriodicExportingMetricReaderFactory::Create(std::move(exporter), options)};
+
+    auto meterProvider{sdk::metrics::MeterProviderFactory::Create()};
     meterProvider->AddMetricReader(std::move(reader));
-    //    meterProvider->AddMetricReader(std::move(stdoutReader));
-    opentelemetry::metrics::Provider::SetMeterProvider(std::move(meterProvider));
+
+    metrics::Provider::SetMeterProvider(std::move(meterProvider));
   }
 
   {
-    const auto apiMetricProvider{opentelemetry::metrics::Provider::GetMeterProvider()};
-    const auto meter{apiMetricProvider->GetMeter("test_meter", "666")};
-    auto counter{meter->CreateUInt64Counter("test_value")};
-    srand(index);
+    using namespace opentelemetry;
+
+    const auto apiMetricProvider{metrics::Provider::GetMeterProvider()};
+    const auto meter{apiMetricProvider->GetMeter("malkia_test_meter", "1.2.3")};
+    auto counter{meter->CreateUInt64Counter("malkia_test_monotonic")};
+    auto udcounter{meter->CreateInt64UpDownCounter("malkia_test_up_down")};
     bool flip{false};
     int period{0};
-    for (size_t i = 0; i < 64; i++)
+    srand(GetCurrentProcessId());
+    for (size_t i = 0; i < 1000; i++)
     {
+      if( i % 100 == 0 )
+      {
+        printf("%zd ", i); fflush(stdout);
+      }
       flip = !flip;
       if (flip)
-        period = rand() % 100;
-      printf(".");
-      counter->Add(1); //, {{"test", "one"}});
-      std::this_thread::sleep_for(std::chrono::milliseconds(flip ? period : 100 - period));
-    }
-  }
-
-  if (const auto mp{std::dynamic_pointer_cast<opentelemetry::sdk::metrics::MeterProvider>(
-          opentelemetry::metrics::Provider::GetMeterProvider())};
-      mp)
-  {
-    if (const auto p{mp.get()}; p)
-    {
-      p->ForceFlush();
-      p->Shutdown();
+        period = rand() % 1000;
+      counter->Add(1);//, {{"test1", "one"}, {"test2", "two"}});
+      if( flip )
+        udcounter->Add(3);//, {{"test1", "one"}, {"test2", "two"}});
+      else
+        udcounter->Add(-1);//, {{"test1", "one"}, {"test2", "two"}});
+      std::this_thread::sleep_for(std::chrono::milliseconds(flip ? period : 1000 - period));
     }
   }
 
